b3b1e1c838dc557248ea4984fc2d5dc56bf57cd5
YawoSadji <yawo.sadji@gmx.com>
2024-11-14 15:49:52 -0500

    modified serializers.py

--- SnipperAPI/api/serializers.py
+++ SnipperAPI/api/serializers.py
@@ -1,11 +1,6 @@
 from rest_framework import serializers
 from .models import User, Snippet
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'email', 'password']
-
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
--- SnipperAPI/api/views.py
+++ SnipperAPI/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Snippet
+from .serializers import SnippetSerializer
 # Create your views here.
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
