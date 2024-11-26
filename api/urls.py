e9e0ec1f0d95174d3fed432cb809dfc1a12cbdff
YawoSadji <yawo.sadji@gmx.com>
2024-11-26 13:27:38 -0500

    added landing page

--- api/urls.py
+++ api/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('add', views.post_snippet),
     path('<int:id>', views.get_single_snippet),
     path('', views.get_all_snippets),
+    # path('<int:id>', views.update_snippet),
+    # path('<int:id>', views.delete_single_snippet),
+    # path('', views.delete_all_snippets),
     # path('language/', views.get_snippet_by_language),
     ]
